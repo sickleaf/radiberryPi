171c58ebf45e1b1bc03b29727dbfdc16916b6c23
sickleaf <5824061+sickleaf@users.noreply.github.com>
2017-10-29 01:17:18 +0900

    [mod/fix] (1)mpv log option (2)input = txt

--- shellScript/playmp3.sh
+++ shellScript/playmp3.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 #$1	resourcePath
 #$2	number(default 100)
+resourcePath="$1"
 number=100
-mp3path="$1"
+exPath=""
+if [ ! "$1" = '' ]; then 
+	resourcePath="$1"
+fi
 if [ ! "$2" = '' ]; then 
 	number="$2"
 fi
-if expr "$mp3path" : '.*\.txt' > /dev/null; then
-	cat "$1" | shuf | head -`expr $2` | while read line; do mpv --no-video "/mnt/tpd1/$line"; done
+
+if expr "$resourcePath" : '.*\.txt' > /dev/null; then
+	echo "<<playmp3.sh>>read_txt"
+	echo "[resourcePath]:$resourcePath"
+	echo "[number]:$number"
+	exPath=`echo $resourcePath | cut -d "/" -f 1-3`
+	echo "[exPath]:$exPath"
+	cat "$resourcePath" | shuf | head -`expr $number` | while read line; do mpv --no-video --msg-level=all=warn "$exPath/$line"; done
 else 
-	find "$1" -name "*.mp3" | shuf | head -`expr $2` | while read line; do mpv --no-video "$line"; done
+	echo "<<playmp3.sh>>specific_dir"
+	echo "[resourcePath]:$resourcePath"
+	echo "[number]:$number"
+	find "$resourcePath" -name "*.mp3" | shuf | head -`expr $number` | while read line; do mpv --no-video --msg-level=all=warn "$line"; done
 fi
